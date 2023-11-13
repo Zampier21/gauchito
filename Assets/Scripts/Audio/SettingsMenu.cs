03654693ffc5f7c03f1e06350c410c2dd90f9601
Zampier21 <weslley.zampier3@gmail.com>
2023-11-13 17:08:57 -0300

    Tentativa de audio e de Banco de Dados

---
+++ Assets/Scripts/Audio/SettingsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingsMenu : MonoBehaviour
+{
+   [SerializeField] private Slider volumeSlider = null;
+   [SerializeField] private Text volumeTextUI = null;
+
+   public void VolumeSlider(float volume){
+        volumeTextUI.text = volume.ToString("0.0");
+   }
+}
